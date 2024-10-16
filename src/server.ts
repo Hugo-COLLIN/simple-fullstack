98a9de7cf73fc5a01cb3f3cd77518dad52f645ae
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-10-16 19:21:23 +0200

    refactor: extract method and constant

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,36 @@
 import {Glob, serve} from "bun";
 import {renderFile} from "pug";
 
+const PAGES_PROJECT_PATH = "./src/views/pages/";
+
+function main() {
+  // Browse all files in the "views" folder
+  const routes: Record<string, (req: Request) => Response> = {};
+  const pages = new Glob(PAGES_PROJECT_PATH + "*.pug");
+
+  for (const file of pages.scanSync(".")) {
+    const routePath = determineRoute(file);
+    routes[routePath] = handlePugRendering(file);
+  }
+
+  console.info(routes);
+
+  // Bun server with dynamic routing
+  serve({
+    fetch(req: Request) {
+      const url = new URL(req.url);
+      const routeHandler = routes[url.pathname];
+      if (routeHandler) {
+        return routeHandler(req);
+      }
+      return new Response("Not Found", {status: 404});
+    },
+    port: 3000,
+  });
+
+  console.log("Server started on http://localhost:3000");
+}
+
 /**
  * Render pug files
  * @param file The Pug file to render
@@ -21,7 +51,7 @@ export function handlePugRendering(file: string) {
 function determineRoute(file: string): string {
   let route = file
     .replace(/\\/g, "/") // Replace folder separators for Windows paths
-    .replace("./src/views/", "")
+    .replace(PAGES_PROJECT_PATH, "")
     .replace(".pug", "");
 
   // If the file is "index.pug", associate to root or subfolder
@@ -34,26 +64,4 @@ function determineRoute(file: string): string {
   return route;
 }
 
-// Browse all files in the "views" folder
-const routes: Record<string, (req: Request) => Response> = {};
-const pages = new Glob("./src/views/*.pug");
-
-for (const file of pages.scanSync(".")) {
-  const routePath = determineRoute(file);
-  routes[routePath] = handlePugRendering(file);
-}
-
-console.info(routes);
-
-// Bun server with dynamic routing
-serve({
-  fetch(req: Request) {
-    const url = new URL(req.url);
-    const routeHandler = routes[url.pathname];
-    if (routeHandler) {
-      return routeHandler(req);
-    }
-    return new Response("Not Found", { status: 404 });
-  },
-  port: 3000,
-});
+main();
